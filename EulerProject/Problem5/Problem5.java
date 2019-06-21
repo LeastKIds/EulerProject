5767fe1859a611b90e44cd221dabd421e3e54c33
LeastKIds <wlsghddl4@naver.com>
2019-06-22 06:42:19 +0900

    190621_2

--- EulerProject/Problem5/Problem5.java
+++ EulerProject/Problem5/Problem5.java
@@ -2,4 +2,35 @@ class Problem5
 {
 	public static void main(String[] args)
 	{
+		int result=1;
 		
+		for(int i=2; i<=19; i++)
+		{
+			result=gcd(result,i);
+		}
+		
+		System.out.println(result);
+	}
+	
+	public static int gcd(int i, int j)
+	{
+		int temp1=i , temp2=j;
+		int r=1;
+		int LCM;
+		
+		if(i<j)
+		{
+			i=temp2;
+			j=temp1;
+		}
+		
+		while(r>0)
+		{
+			r=i%j;
+			i=j;
+			j=r;
+		}
+		LCM=temp1*temp2/i;
+		return LCM;
+	}
+}
