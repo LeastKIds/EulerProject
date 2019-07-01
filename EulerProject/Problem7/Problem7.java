5c9c810639d482bfd7fadfbae0daa6d1d0839829
LeastKIds <leastkids@gmail.com>
2019-07-01 18:19:54 +0900

    190701_1

---
+++ EulerProject/Problem7/Problem7.java
@@ -0,0 +1,12 @@
+class Problem7
+{
+  public static void main(String[] args)
+  {
+    int[] prime=new int[1];
+    prime[0]=2;
+    for(int i=3; print.length<=10001; i++)
+    {
+      
+    }
+  }
+}
