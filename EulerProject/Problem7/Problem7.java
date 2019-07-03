50841540d29def73f82fae5aa1b486350e6bd3f7
LeastKIds <leastkids@gmail.com>
2019-07-03 09:31:08 +0900

    190702

--- EulerProject/Problem7/Problem7.java
+++ EulerProject/Problem7/Problem7.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+
 class Problem7
 {
   public static void main(String[] args)
   {
-    int[] prime=new int[1];
-    prime[0]=2;
-    for(int i=3; print.length<=10001; i++)
+    final int result=10001;
+    ArrayList<Integer> prime=new ArrayList<>();
+    prime.add(2);
+    for(int i=3; prime.size()<=result; i++)
     {
-      
+      for(int j=0; j<prime.size();j++)
+      {
+        if(i%(prime.get(j))==0)
+            break;
+        else  if((j+1)==prime.size())
+          prime.add(i);
+      }
     }
+    System.out.println(prime.get(result-1));
   }
 }
