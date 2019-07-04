5f0c44b2d35d3c890df8c0891b4fea869db9e065
LeastKIds <leastkids@gmail.com>
2019-07-05 07:00:04 +0900

    190704

--- EulerProject/Problem10/Problem10.java
+++ EulerProject/Problem10/Problem10.java
@@ -4,21 +4,20 @@ class Problem10
 {
   public static void main(String[] args)
   {
-    int sum=2;
+    long sum=2;
     ArrayList<Integer> prime=new ArrayList<>();
     prime.add(2);
-    for(int i=3; i<=10; i++)
+    for(int i=3; i<2000000; i++)
     {
-      for(int j=0; j<prime.size(); j++)
+      for(int j=0; j<prime.size(); j++ )
       {
         if(i%prime.get(j)==0)
-        {
           break;
-        }
         else if((j+1)==prime.size())
         {
           prime.add(i);
-          sum+=i;
+          sum+=(long)i;
+          break;
         }
       }
     }
