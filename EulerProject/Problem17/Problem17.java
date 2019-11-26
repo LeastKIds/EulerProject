8f371c35a797ea32425c1b2adc93274d88a923b5
LeastKIds <leastkids@gmail.com>
2019-11-26 21:56:27 +0900

    191126

--- EulerProject/Problem17/Problem17.java
+++ EulerProject/Problem17/Problem17.java
@@ -3,24 +3,61 @@ class Problem17
   public static void main(String[] args)
   {
     int sum=0, temporarily=0;
-    String thousand="thousand"; 
-    int[] figure=new int[4];
+    int figure;
 
-    for(int i=0; i<=1000; i++)
+    // one~twenty letter length setting
+    String[] one_twenty = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve",
+  "Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen","Twenty"};
+    int[] one_twenty_length = new int[20];
+    for(int i=0; i<20; i++)
+      one_twenty_length[i]=one_twenty[i].length();
+
+    // tenth digit letter length setting
+    String[] twenty_tenthDigit = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    int[] twenty_tenthDigit_length = new int[8];
+    for(int i=0; i<8; i++)
+      twenty_tenthDigit_length[i]=twenty_tenthDigit[i].length();
+
+    // hundred letter length setting
+    String hundred="hundred";
+    int hundred_length=hundred.length();
+
+    // thousand letter length setting
+    String thousand="thousand";
+    int thousand_length=thousand.length();
+
+    // remind letter setting
+    int and_length=3;
+
+    for(int i=1; i<=1000; i++)
     {
       temporarily=i;
-      for(int j=0; j<4; j++)
-      {
-        figure[j]=temporarily%10;
-        temporarily/=10;
-      }
+      if(i==1000)  { figure=4; }
+      else if(i>99) { figure=3; }
+      else { figure=2; }
 
-      if(figure[3]!=0)
-        sum+=thousand.length();
-      else if(figure[2]!=0)
+      switch(figure)
       {
+        case 4:
+          sum+=thousand_length; sum+=one_twenty_length[0];  break;
+        case 3:
+          sum+=one_twenty_length[temporarily/100-1]; sum+=hundred_length;
+          if(temporarily%100==0)  { break;  }
+          sum+=and_length;
+          temporarily-=(temporarily/100)*100;
+
+        case 2:
+          if(temporarily>20)
+          {
+            sum+=twenty_tenthDigit_length[temporarily/10-2];
+            if(temporarily%10==0) { break;  }
+            sum+=one_twenty_length[temporarily%10-1];
+          }
+          else
+            sum+=one_twenty_length[temporarily-1];
 
       }
     }
+    System.out.println(sum);
   }
 }
