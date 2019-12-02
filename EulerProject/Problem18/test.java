565e081ee5a8bb9be70786e1ba1b1a7e65bab4ba
LeastKIds <leastkids@gmail.com>
2019-12-02 16:31:42 +0900

    191202

--- EulerProject/Problem18/test.java
+++ EulerProject/Problem18/test.java
@@ -2,56 +2,46 @@ class test
 {
   public static void main(String[] args)
   {
-    int[][] pyramid=new int[4][];
-    pyramid[0]=new int[] {3};
-    pyramid[1]=new int[] {7,4};
-    pyramid[2]=new int[] {2,4,6};
-    pyramid[3]=new int[] {8,5,9,3};
+    String number="75 95 64 17 47 82 18 35 87 10 20 04 82 47 65 19 01 23 75 03 34";
+    String[] singleNumber=number.split("\\s");
 
-    final int FLOOR=4;
+    final int FLOOR=6;
+    int[][] pyramid=new int[FLOOR][];
+    for(int i=0; i<FLOOR; i++)
+      pyramid[i]=new int[i+1];
 
-//    for(int i=0; i<FLOOR; i++)
-//    {
-//      for(int j=0; j<=i; j++)
-//        System.out.print(pyramid[i][j] + " ");
-//      System.out.println();
-//    }
-
-    int sum=0, max=0;
-    int n;
-    int[] fork=new int[FLOOR-1];
-
-    boolean special=false;
-    for(int i=1; i<=4; i++)
-    {
-      sum=0;
-      n=FLOOR-i;
-
-      //fork rollback
-      for(int k=0; k<FLOOR-1; k++)
-        fork[k]=0;
-
-      for(int j=FLOOR-1; j>=0; j--)
+    int singleNumberStack=0;
+    for(int i=0; i<FLOOR; i++)
+        for(int j=0; j<=i; j++)
       {
-        if(n==j || n==0)
-          special=true;
-        else if(fork[j-1]!=0)
-          n-=1;
-        else
-          fork[j-1]=1;
-
-        System.out.println(pyramid[j][n]);
-        sum+=pyramid[j][n];
-        if(special && n!=0)
-          n--;
+        pyramid[i][j]=Integer.parseInt((singleNumber[singleNumberStack]));
+        singleNumberStack++;
       }
-      System.out.println();
 
-      if(max<sum)
-        max=sum;
-      special=false;
+    int[][] route=new int[FLOOR-3][(FLOOR-3)*2];
+    int count=0;
+    boolean first=true;
+    for(int i=FLOOR-2; i>1; i--)
+    {
+      count=0;
+      for(int j=1; j<i; j++)
+      {
+        if(first)
+        {
+          route[i-2][count]=pyramid[i][j]+pyramid[i+1][j];
+          System.out.println(route[i-2][count] + " " + count);
+          count++;
+          route[i-2][count]=pyramid[i][j]+pyramid[i+1][j+1];
+          System.out.println(route[i-2][count] + " " + count);
+          count++;
+        }
+        else
+        {
+          route[i-2][count]=route[i-1][count]+route[i-1][count+1];
+        }
+      }
+      first=false;
     }
 
-    System.out.println(max);
   }
 }
---
+++ EulerProject/Problem18/test1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class test1
+{
+  public static void main(String[] args)
+  {
+    
+  }
+}
