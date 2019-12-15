35a9ec9159df244e3ed62ee076a5b67aa429c98b
LeastKIds <leastkids@gmail.com>
2019-12-15 21:30:59 +0900

    191215

--- EulerProject/Problem27/Problem27.java
+++ EulerProject/Problem27/Problem27.java
@@ -5,7 +5,7 @@ class Problem27
 {
   public static void main(String[] args)
   {
-    final int MAXINT=1000;
+    final int MAXINT=100;
     ArrayList<Integer> primeB=new ArrayList<>();
     primeB.add(2);
     int primeBSize;
@@ -22,80 +22,41 @@ class Problem27
         }
     }
 
+    ArrayList<Integer> prime=new ArrayList<>();
+    prime=primeB;
+
     ArrayList<Integer> primeA=new ArrayList<>();
     for(int i=(-MAXINT)+1; i<MAXINT; i++)
       primeA.add(i);
-
+    System.out.println(primeA);
 
     int n=1;
     int maxAB=0;
     int result=0;
     int primeASize;
-    loop: while(true)
+    loof: while(true)
     {
-      primeASize=primeA.size();
-      primeBSize=primeB.size();
-      for(int a=0; a<primeASize; a++)
-      {
-        for(int b=0; b<primeBSize; b++)
+
+      for(int b=0; b<primeB.size(); b++)
+        for(int a=0; a<primeA.size(); a++)
         {
-          result=n*n+n*primeA.get(a)+primeB.get(b);
+          result=n*(n+primeA.get(a))+primeB.get(b);
           System.out.println("primeA : " + primeA.get(a));
           System.out.println("primeB : " + primeB.get(b));
           System.out.println("result : " + result);
           System.out.println("n : " + n);
-          if(result<=0)
-          {
+          System.out.println();
+          if(result<=0 || prime.contains(result))
             primeA.remove(a);
-            break;
-          }
-          else if (!primeCheck(result))
-          {
-            primeA.remove(a);
-            break;
-          }
-
-          if(primeA.size()==1)
-          {
-            maxAB=primeA.get(a)*primeB.get(b);
-            break loop;
-          }
+          else
+            prime.add(result);
         }
-      }
-
-      n++;
+      break;
     }
 
     System.out.println(maxAB);
 
   }
 
-  public static boolean primeCheck(int result)
-  {
-    boolean check=false;
-    ArrayList<Integer> primeCheck=new ArrayList<>();
-    primeCheck.add(2);
-    int primeCheckSize;
-    for(int i=3; i<result; i++)
-    {
-      primeCheckSize=primeCheck.size();
-      for(int j=0; j<primeCheckSize; j++)
-      {
-        if(i%primeCheck.get(j)==0)
-        {
-          check=false;
-          break;
-        }
-        else if(primeCheck.get(j)>Math.sqrt(i))
-        {
-          check=true;
-          break;
-        }
-      }
-    }
-
-    return check;
-
-  }
 
 }
