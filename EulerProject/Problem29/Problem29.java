220206073301934ba8b53f67ead23385d706ae03
LeastKIds <leastkids@gmail.com>
2019-12-26 16:43:08 +0900

    191226_2

--- EulerProject/Problem29/Problem29.java
+++ EulerProject/Problem29/Problem29.java
@@ -1,26 +1,30 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.math.BigInteger;
 
 class Problem29
 {
   public static void main(String[] args)
   {
-    ArrayList<BigInteger> pow=new ArrayList<>();
+    HashSet<BigInteger> pow=new HashSet<>();
     final int smallNum=2;
     final int bigNum=100;
 
-    BigInteger aBigInteger=new BigInteger("");
-    BigInteger bBigInteger=new BigInteger("");
+    BigInteger aBigInteger=new BigInteger("0");
+    BigInteger result=new BigInteger("0");
 
-    
+    String aString,bString;
     for(int a=smallNum; a<=bigNum; a++)
     {
       for(int b=smallNum; b<=bigNum; b++)
       {
+        aBigInteger=new BigInteger(Integer.toString(a));
+        result=aBigInteger.pow(b);
+        pow.add(result);
 
       }
     }
 
+    System.out.println(pow.size());
 
   }
 }
