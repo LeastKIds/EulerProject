ebf9deac9afa64db3f2a27152afd21d4e71c864a
LeastKIds <leastkids@gmail.com>
2020-01-09 14:24:37 +0900

    200109

--- EulerProject/Problem31/Problem31.java
+++ EulerProject/Problem31/Problem31.java
@@ -2,44 +2,61 @@ class Problem31
 {
   public static void main(String[] args)
   {
-      final int MAKE_POUND=200;
+      final int MAKE_POUND=10;
       int[] combination=new int[]{100,50,20,10,5,2};
       int combinationLength=combination.length;
-      int count=2;
-      int digit_count_100=0,digit_count_50=0,digit_count_20=0,
-      digit_count_10=0,digit_count_5=0,digit_count_2=0;
-      int remind,result;
-      final int MAX_2=MAKE_POUND/combination[5];
-      final int MAX_5=MAKE_POUND/combination[4];
-      final int MAX_10=MAKE_POUND/combination[3];
-      final int MAX_20=MAKE_POUND/combination[2];
-      final int MAX_50=MAKE_POUND/combination[1];
-      final int MAX_100=MAKE_POUND/combination[0];
+      int count=0;
 
-      for(int num_100=0; num_100<=MAX_100; num_100++)
-      {
-        for(int num_50=0; num_50<=MAX_50; num_50++)
-        {
-          for(int num_20=0; num_20<=MAX_20; num_20++)
-          {
-            for(int num_10=0; num_10<=MAX_10; num_10++)
-            {
-              for(int num_5=0; num_5<=MAX_5; num_5++)
-              {
-                for(int num_2=1; num_2<=MAX_2; num_2++)
-                {
-                  result=combination[5]*num_2 + combination[4]*num_5 + combination[3]*num_10 + combination[2]*num_20 + combination[1]*num_50 + combination[0]*num_100;
-                  if(result>MAKE_POUND)
-                    break;
-                  count++;
-                }
-              }
-            }
-          }
-        }
-      }
-
+      for(int i=combinationLength-1; i>=0; i--)
+        count=switchLoop(MAKE_POUND,combination[i],count,i);
+      //count=switchLoop(MAKE_POUND,2,count,5);
+      //count=switchLoop(MAKE_POUND,5,count,4);
       System.out.println(count);
 
+  }
+
+  public static int switchLoop(final int MAKE_POUND, int combination, int count, int digit)
+  {
+    final int MAX_DIGIT=MAKE_POUND/combination;
+    int digit_count_100=0,digit_count_50=0,digit_count_20=0,
+    digit_count_10=0,digit_count_5=0,digit_count_2=0;
+    switch(digit)
+    {
+      case 5: break;
+      case 4: digit_count_5=1;
+              break;
+      case 3: digit_count_10=1;
+              break;
+      case 2: digit_count_20=1;
+              break;
+      case 1: digit_count_50=1;
+              break;
+      case 0: digit_count_100=1;
+              break;
+    }
+
+    switch(combination)
+    {
+      case 2: count+=MAX_DIGIT;
+              //System.out.println(count);
+              break;
+      case 5: for(int i=digit_count_5; i<=MAX_DIGIT; i++)
+                count=switchLoop(MAKE_POUND-(i*5),2,count,digit)+1;
+              //System.out.println(count);
+              break;
+      case 10: for(int i=digit_count_10; i<=MAX_DIGIT; i++)
+                count=switchLoop(MAKE_POUND-(i*10),5,count,digit)+1;
+              break;
+      case 20: for(int i=digit_count_20; i<=MAX_DIGIT; i++)
+                count=switchLoop(MAKE_POUND-(i*20),10,count,digit)+1;
+              break;
+      case 50: for(int i=digit_count_50; i<=MAX_DIGIT; i++)
+                count=switchLoop(MAKE_POUND-(i*50),20,count,digit)+1;
+              break;
+      case 100: for(int i=digit_count_100; i<=MAX_DIGIT; i++)
+                  count=switchLoop(MAKE_POUND-(i*100),50,count,digit)+1;
+              break;
+    }
+    return count;
   }
 }
