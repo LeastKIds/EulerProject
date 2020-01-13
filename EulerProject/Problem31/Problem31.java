f69c263c849a87cbce3efb1992f03276f2059975
LeastKIds <leastkids@gmail.com>
2020-01-13 16:10:22 +0900

    200113

--- EulerProject/Problem31/Problem31.java
+++ EulerProject/Problem31/Problem31.java
@@ -2,10 +2,10 @@ class Problem31
 {
   public static void main(String[] args)
   {
-      final int MAKE_POUND=10;
+      final int MAKE_POUND=200;
       int[] combination=new int[]{100,50,20,10,5,2};
       int combinationLength=combination.length;
-      int count=0;
+      int count=1;
 
       for(int i=combinationLength-1; i>=0; i--)
         count=switchLoop(MAKE_POUND,combination[i],count,i);
@@ -37,25 +37,30 @@ class Problem31
 
     switch(combination)
     {
-      case 2: count+=MAX_DIGIT;
+      case 2: count+=MAX_DIGIT+1;
               //System.out.println(count);
               break;
       case 5: for(int i=digit_count_5; i<=MAX_DIGIT; i++)
-                count=switchLoop(MAKE_POUND-(i*5),2,count,digit)+1;
+                count=switchLoop(MAKE_POUND-(i*5),2,count,digit);
+              //count+=1;
               //System.out.println(count);
               break;
       case 10: for(int i=digit_count_10; i<=MAX_DIGIT; i++)
-                count=switchLoop(MAKE_POUND-(i*10),5,count,digit)+1;
+                count=switchLoop(MAKE_POUND-(i*10),5,count,digit);
+              //count+=1;
               break;
       case 20: for(int i=digit_count_20; i<=MAX_DIGIT; i++)
-                count=switchLoop(MAKE_POUND-(i*20),10,count,digit)+1;
+                count=switchLoop(MAKE_POUND-(i*20),10,count,digit);
+              //count+=1;
               break;
       case 50: for(int i=digit_count_50; i<=MAX_DIGIT; i++)
-                count=switchLoop(MAKE_POUND-(i*50),20,count,digit)+1;
+                count=switchLoop(MAKE_POUND-(i*50),20,count,digit);
+              //count+=1;
               break;
       case 100: for(int i=digit_count_100; i<=MAX_DIGIT; i++)
-                  count=switchLoop(MAKE_POUND-(i*100),50,count,digit)+1;
-              break;
+                  count=switchLoop(MAKE_POUND-(i*100),50,count,digit);
+                //count+=1;
+                break;
     }
     return count;
   }
