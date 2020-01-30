4cd7c83614a8a6dfbbceb0791b93fb25c8e4e555
LeastKIds <leastkids@gmail.com>
2020-01-30 17:11:54 +0900

    200130

---
+++ EulerProject/Problem32/test_2.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+
+class test_2
+{
+  public static void main(String[] args)
+  {
+    int n=9;
+    int r=5;
+
+    LinkedList<Integer> perArr=new LinkedList<Integer>();
+    int[] perCheck=new int[n];
+    permutation(n,r,perArr,perCheck);
+  }
+
+  public static void permutation(int n, int r, LinkedList<Integer> perArr, int[] perCheck)
+  {
+    if(perArr.size()==r)
+    {
+      for(int i : perArr)
+        System.out.print(i+" ");
+      System.out.println();
+      return;
+    }
+
+    for(int i=1; i<=n; i++)
+      if(perCheck[i-1]==0)
+      {
+        perArr.add(i);
+        perCheck[i-1]=1;
+        permutation(n,r,perArr,perCheck);
+        perCheck[i-1]=0;
+        perArr.removeLast();
+      }
+  }
+}
